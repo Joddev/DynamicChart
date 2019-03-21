94f1b7b040dde5079d595012e1153f3de1b9f5db
chy <whgksdyd112@naver.com>
2019-03-21 14:13:14 +0900

    Add JsDoc

--- src/components/DynamicChart.vue
+++ src/components/DynamicChart.vue
@@ -23,7 +23,7 @@ function animate (time) {
 requestAnimationFrame(animate)
 
 export default {
-  name: 'varative-chart',
+  name: 'DynamicChart',
   props: {
     interval: {
       type: Number,
@@ -103,7 +103,8 @@ export default {
       blurMap: {},
       scaleList: [],
       scaleUnit: 1,
-      nullNumber: -12345
+      nullNumber: -12345,
+      iconOnBarSize: 70
     }
   },
   created: function () {
@@ -111,6 +112,7 @@ export default {
   },
   methods: {
     setAdditionalInfo: function (list, amount) {
+      // TODO complete additional info
       const tbody = document.getElementById('additional-info')
       const count = tbody.childElementCount
       const need = amount - count
@@ -121,50 +123,98 @@ export default {
         rows[i].innerHTML = `<span class="additional-label">${label}:</span> ${this.elementMap[label].additional} ${this.additionalUnit}`
       }
     },
-    getNodeValue: function (node) {
-      const label = node.children[0].textContent
-      // console.log(`${label}: ${this.elementMap[label].value}`)
-      return this.elementMap[label].value
+    /**
+     * get label from node
+     * @param {HTMLElement} node
+     * @return {string} - node label
+     */
+    $_getNodeLabel: function (node) {
+      return node.children[0].textContent
     },
-    getWidth: function (cur, max, min) {
+    /**
+     * get value from node
+     * @param {HTMLElement} node
+     * @return {number} - node value
+     */
+    $_getNodeValue: function (node) {
+      return this.elementMap[this.$_getNodeLabel(node)].value
+    },
+    /**
+     * get bar HTMLElement from node
+     * @param {HTMLElement} node
+     * @return {HTMLElement} - bar element
+     */
+    $_getNodeBar: function (node) {
+      return node.children[1]
+    },
+    /**
+     * get label on bar HTMLElement from node
+     * @param {HTMLElement} node
+     * @return {HTMLElement}
+     */
+    $_getNodeLabelOnBar: function (node) {
+      return node.children[2].children[0].children[0]
+    },
+    /**
+     * get icon on bar HTMLElement from node
+     * @param {HTMLElement} node
+     * @return {HTMLElement}
+     */
+    $_getNodeIconOnBar: function (node) {
+      return node.children[2].children[0].children[1]
+    },
+    /**
+     * get bar width
+     * front width is affected by current max value
+     * back width is affected by total max value
+     * @param {number} cur - target value
+     * @param {number} max - max value
+     * @param {number} [min] - min value
+     */
+    $_getWidth: function (cur, max, min = 0) {
       const maximum = Math.max(max, this.maximum)
-      const maxWidth = 1400
-      const tailWidth = 400
-      if (this.dynamic) {
-        return Number((((cur - min) / (max - min)) * maxWidth + (cur / maximum) * tailWidth).toFixed(2))
-      } else {
-        return Number(((cur / max) * maxWidth + (cur / maximum) * tailWidth).toFixed(2))
-      }
+      const frontWidth = 1400
+      const backWidth = 400
+      if (this.dynamic) return (((cur - min) / (max - min)) * frontWidth + (cur / maximum) * backWidth)
+      else return ((cur / max) * frontWidth + (cur / maximum) * backWidth)
     },
-    setWidth: function () {
-      const newList = this.elementList.concat().sort((n1, n2) => {
-        return this.getNodeValue(n2) - this.getNodeValue(n1)
+    /**
+     * set bar width by current value
+     */
+    $_setWidth: function () {
+      const elementListSorted = this.elementList.slice().sort((n1, n2) => {
+        return this.$_getNodeValue(n2) - this.$_getNodeValue(n1)
       })
-      // console.log(newList)
-      const max = this.getNodeValue(newList[0])
-      const min = this.dynamic ? this.getNodeValue(newList[this.limit - 1]) * 0.9 : 0
+      const topElement = elementListSorted[0]
+      const max = this.$_getNodeValue(topElement)
+      const min = this.dynamic ? this.$_getNodeValue(elementListSorted[this.limit - 1]) * 0.9 : 0
       const margin = this.limit === 15 ? 55 : 70
-      document.getElementsByClassName('representative')[0].style.backgroundImage = `url('${this.labelInfo[newList[0].children[0].textContent].img}')`
-      if (this.getNodeValue(newList[0]) === this.getNodeValue(newList[1])) this.elementMap[newList[0].children[0].textContent].value += 0.001
-      for (let i = 0; i < newList.length; i++) {
-        const element = newList[i]
+      // set representative image
+      document.getElementsByClassName('representative')[0].style.backgroundImage = `url('${this.labelInfo[this.$_getNodeLabel(topElement)].img}')`
+      // to handle equal value problems
+      if (this.$_getNodeValue(topElement) === this.$_getNodeValue(elementListSorted[1])) this.elementMap[this.$_getNodeLabel(topElement)].value += 0.001
+      // set width each
+      for (let i = 0; i < elementListSorted.length; i++) {
+        const element = elementListSorted[i]
         if (i < this.limit) {
-          const width = this.getWidth(this.getNodeValue(element), max, min)
-          element.children[1].style.width = width + 'px'
+          const width = this.$_getWidth(this.$_getNodeValue(element), max, min)
+          this.$_getNodeBar(element).style.width = width + 'px'
+          // label on bar exceeds bar size
           if (width - margin < element.children[2].children[0].offsetWidth) {
-            element.children[2].children[0].children[0].style.opacity = 0
-          } else if (element.children[2].children[0].children[0].style.opacity === '0' && width - margin >= element.children[2].children[0].offsetWidth) {
-            element.children[2].children[0].children[0].style.opacity = 1
+            this.$_getNodeLabelOnBar(element).style.opacity = 0
+          } else if (this.$_getNodeLabelOnBar(element).style.opacity === '0') {
+            this.$_getNodeLabelOnBar(element).style.opacity = 1
           }
-          if (width < 70) element.children[2].children[0].children[1].style.opacity = 0
-          else element.children[2].children[0].children[1].style.opacity = 1
-          if (element.style.display !== 'table') {
-            element.style.display = 'table'
-          }
+          // icon on bar excceds bar size
+          if (width < 70) this.$_getNodeIconOnBar(element).style.opacity = 0
+          else this.$_getNodeIconOnBar(element).style.opacity = 1
+          // set bar visible
+          if (element.style.display !== 'table') element.style.display = 'table'
         } else {
-          const label = element.children[0].textContent
+          const label = this.$_getNodeLabel(element)
           if (element.style.display !== 'none') {
             element.style.opacity = 0
+            // set bar invisible
             if (!this.blurMap[label]) {
               this.blurMap[label] = setTimeout(() => {
                 element.style.display = 'none'
@@ -174,14 +224,12 @@ export default {
           }
         }
       }
-      if (this.additional) this.setAdditionalInfo(newList, this.additionalLimit)
+      if (this.additional) this.setAdditionalInfo(elementListSorted, this.additionalLimit)
+      // set scale
       const leftMargin = this.limit === 15 ? 215 : 285
-      let start = this.scaleUnit
-      if (this.scaleList.length) {
-        start = this.scaleList[this.scaleList.length - 1].value + this.scaleUnit
-      }
+      const start = this.scaleList.length ? this.scaleList[this.scaleList.length - 1].value + this.scaleUnit : this.scaleUnit
       for (let value = start; value < max * 1.1; value += this.scaleUnit) {
-        const element = this.createScale(value)
+        const element = this.$_createScale(value)
         this.scaleList.push({
           value: value,
           node: element
@@ -195,29 +243,51 @@ export default {
         })
         this.scaleList = this.scaleList.filter(scale => scale.value % this.scaleUnit === 0)
       }
-      for (let i = 0; i < this.scaleList.length; i++) {
-        const scale = this.scaleList[i]
+      for (const scale of this.scaleList) {
         if (scale.value > max * 1.1 || scale.value < min) scale.node.style.display = 'none'
         else {
           scale.node.style.display = ''
-          scale.node.style.left = Number(this.getWidth(scale.value, max, min)) + leftMargin + 'px'
+          scale.node.style.left = Number(this.$_getWidth(scale.value, max, min)) + leftMargin + 'px'
         }
       }
     },
-    createScale: function (num) {
+    /**
+     * create scale element
+     * @param {number} num - scale value
+     * @return {HTMLElement} scale element
+     */
+    $_createScale: function (num) {
       const div = document.createElement('div')
       div.innerHTML = `
         <div id="scale-${num}" class="scale">
           <div class="scale-bar"></div>
-          <div class="scale-value">${this.numberWithCommas(num) + this.unit}</div>
+          <div class="scale-value">${this.$_numberWithCommas(num) + this.unit}</div>
         </div>
       `
       return div.firstElementChild
     },
-    setNodeValue: function (node, value) {
-      node.children[3].innerHTML = this.numberWithCommas(value.toFixed(this.fixed)) + this.unit
+    /**
+     * get node value label
+     * @param {HTMLElement} node
+     * @return {HTMLElement} node value label
+     */
+    $_getNodeValueLabel: function (node) {
+      return node.children[3]
     },
-    createNode: function (data) {
+    /**
+     * set node value label
+     * @param {HTMLElement} node
+     * @param {number} value
+     */
+    $_setNodeValueLabel: function (node, value) {
+      this.$_getNodeValueLabel(node).innerHTML = this.$_numberWithCommas(value.toFixed(this.fixed)) + this.unit
+    },
+    /**
+     * create node element
+     * @param {{label: string, value: (number|string)}} data
+     * @return {HTMLElement} created node element
+     */
+    $_createNode: function (data) {
       var div = document.createElement('div')
       let color, img
       try {
@@ -225,7 +295,7 @@ export default {
         img = this.labelInfo[data.label].img
       } catch (err) {
         console.log(data.label)
-        color = this.getRandomColor()
+        color = this.$_getRandomColor()
         img = 'https://banner2.kisspng.com/20171216/0a6/question-mark-png-5a352b58b02c08.4921308315134339447216.jpg'
       }
       div.innerHTML = `
@@ -233,7 +303,7 @@ export default {
             <div class="item-label limit-${this.limit}">${data.label}</div>
             <div class="item-bar limit-${this.limit}" style="background: ${color}"></div>
             <div class="item-icon limit-${this.limit}"><div class="item-marker"><span>${data.label}</span><img src="${img}"></div></div>
-            <div class="item-value limit-${this.limit}">${this.numberWithCommas(data.value.toFixed(this.fixed))}</div>
+            <div class="item-value limit-${this.limit}">${this.$_numberWithCommas(data.value.toFixed(this.fixed))}</div>
           </div>
       `.trim()
 
@@ -241,28 +311,41 @@ export default {
       // Change this to div.childNodes to support multiple top-level nodes
       return div.firstChild
     },
-    createElement: function (data) {
+    /**
+     * create node element
+     * @param {{label: string, value: (number|string)}} data
+     * @return {object} created node element object
+     */
+    $_createElement: function (data) {
       if (data.value !== '-') {
-        data.value = Number(Number(data.value).toFixed(this.fixed))
+        data.value = Number(data.value)
         this.elementMap[data.label] = {
           value: data.value,
-          node: this.createNode(data),
+          node: this.$_createNode(data),
           additional: data.additional
         }
         const node = this.elementMap[data.label].node
+        // register to shuffle object
         this.instance.element.appendChild(node)
         this.instance.add([node])
       }
     },
-    getRandomColor: function () {
-      var letters = '0123456789ABCDEF'
-      var color = '#'
+    /**
+     * @return {string} color
+     */
+    $_getRandomColor: function () {
+      const letters = '0123456789ABCDEF'
+      let color = '#'
       for (var i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)]
       }
       return color
     },
-    numberWithCommas: function (x) {
+    /**
+     * number comma by thousands
+     * @param {number} x
+     */
+    $_numberWithCommas: function (x) {
       return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
     }
   },
@@ -271,22 +354,22 @@ export default {
       itemSelector: '.item',
       speed: this.shuffleSpeed
     })
-
+    // set initial data
     document.getElementById('year').textContent = this.date[0]
     for (const i in this.stats[0]) {
       const curData = this.stats[0][i]
-      this.createElement(curData)
+      this.$_createElement(curData)
     }
-
     this.instance.sort({
       by: (element) => {
-        return this.getNodeValue(element)
+        return this.$_getNodeValue(element)
       },
       reverse: true
     })
+
     // to block initial label overflow
-    this.setWidth()
-    this.setWidth()
+    this.$_setWidth()
+    this.$_setWidth()
 
     let index = 1
     setTimeout(() => {
@@ -304,23 +387,25 @@ export default {
                   label: curData.label,
                   value: 0
                 }
-                this.createElement(initial)
+                this.$_createElement(initial)
                 prevData = this.elementMap[curData.label]
               }
               if (curData.value !== '-') {
                 let textColor = '#FFF'
-                if (prevData.value > curData.value) textColor = '#ff0000'
+                if (prevData.value > curData.value) textColor = '#F00'
+                // if there is no prev data, set value label immediately
                 if (prevData.value === this.nullNumber) {
                   prevData.value = curData.value
-                  this.setNodeValue(prevData.node, Number(curData.value))
+                  this.$_setNodeValueLabel(prevData.node, Number(curData.value))
                 }
                 fromTweening[curData.label] = prevData.value
                 toTweening[curData.label] = Number(curData.value)
-                prevData.node.children[3].style.color = textColor
+                this.$_getNodeValueLabel(prevData.node).style.color = textColor
               } else {
                 prevData.value = this.nullNumber
               }
             }
+            // set null unranked elements
             for (const label in this.elementMap) {
               if (!fromTweening[label]) this.elementMap[label].value = this.nullNumber
             }
@@ -331,17 +416,18 @@ export default {
                 for (const label in fromTweening) {
                   const value = fromTweening[label]
                   this.elementMap[label].value = value
-                  this.setNodeValue(this.elementMap[label].node, value)
+                  this.$_setNodeValueLabel(this.elementMap[label].node, value)
                 }
-                this.setWidth()
+                this.$_setWidth()
               })
               .start()
           } else {
+            // TODO
             // initialize
             for (const name in this.elementMap) {
               this.elementMap[name].value = 0
               this.elementMap[name].addtional = 0
-              this.setNodeValue(this.elementMap[name].node, 0)
+              this.$_setNodeValueLabel(this.elementMap[name].node, 0)
             }
             for (const i in this.stats[index]) {
               const curData = this.stats[index][i]
@@ -352,14 +438,14 @@ export default {
                   value: 0,
                   additional: 0
                 }
-                this.createElement(initial)
+                this.$_createElement(initial)
                 prevData = this.elementMap[curData.label]
               }
               prevData.value = Number(curData.value)
               prevData.additional = Number(curData.additional)
-              this.setNodeValue(prevData.node, Number(curData.value))
+              this.$_setNodeValueLabel(prevData.node, Number(curData.value))
             }
-            this.setWidth()
+            this.$_setWidth()
           }
           // additional
           if (this.additionalStats.length > 0) {
@@ -375,7 +461,7 @@ export default {
     setInterval(() => {
       this.instance.sort({
         by: (element) => {
-          return this.getNodeValue(element)
+          return this.$_getNodeValue(element)
         },
         reverse: true
       })
